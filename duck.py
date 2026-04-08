b49e843b65537372b80034282e70a6af6fe14c16
whitekotan0 <gcat5770@gmail.com>
2026-04-08 20:35:30 +0300

    chore: add newline at the end of the file

--- duck.py
+++ duck.py
@@ -40,5 +40,4 @@ class Duck:
     def is_hit(self, pos):
         duck_rect = pygame.Rect(self.x, self.y, 50, 50)
         return duck_rect.collidepoint(pos)
-    
-    
+
--- game.py
+++ game.py
@@ -179,5 +179,4 @@ class Game:
 
         pygame.quit()
         sys.exit()
-
-        
+      
