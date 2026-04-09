4d144e125d04af210477ae2ddc2f12bffec66958
Ivan Koshelia <koshelia13112006@gmail.com>
2026-04-09 21:13:21 +0300

    fast fix

--- tests/test_game2.py
+++
@@ -1,9 +0,0 @@
-def test_game_init():
-    from unittest.mock import patch
-
-    with patch("game.pygame.mixer.init"), \
-         patch("game.pygame.mixer.Sound"):
-        from game import Game
-        game = Game()
-
-        assert game.state == "menu"
