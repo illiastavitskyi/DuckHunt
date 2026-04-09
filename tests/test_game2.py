e1848bc81e0f7a4b44c48cfdbcc27dd7f6be6338
Ivan Koshelia <koshelia13112006@gmail.com>
2026-04-09 20:21:26 +0300

    fixed linter warnings

--- tests/test_game2.py
+++ tests/test_game2.py
@@ -1,5 +1,8 @@
 def test_game_init():
-    from game import Game
+    from unittest.mock import patch
 
-    game = Game()
-    assert game.state == "menu"
+    with patch("pygame.mixer.init"):
+        from game import Game
+        game = Game()
+
+        assert game.state == "menu"
