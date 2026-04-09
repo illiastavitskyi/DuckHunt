db05af70137bdd09ccfb1810ae4a23998d3e3e53
Ivan Koshelia <koshelia13112006@gmail.com>
2026-04-09 21:05:55 +0300

    fixed linter warnings

--- tests/test_game2.py
+++ tests/test_game2.py
@@ -1,7 +1,7 @@
 def test_game_init():
     from unittest.mock import patch
 
-    with patch("pygame.mixer.init"):
+    with patch("pygame.mixer.init"), patch("pygame.mixer.Sound"):
         from game import Game
         game = Game()
 
